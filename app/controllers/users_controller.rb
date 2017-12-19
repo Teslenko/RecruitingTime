150e093611aa9a0de38bccacd4bdc156fea9f365
root <root@DESKTOP-J7RCC6J.localdomain>
2017-12-19 22:50:13 +0200

    make it better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,48 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate_user!, except => [:show, :index]
+  # before_action :authenticate_user!, :except => [:index]
+  # before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
+  before_filter :signed_in_user, only: [:edit, :update]
+  
   def index
     # do something
     @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 
   def show
     # do something
   end
 
-  def create
-    # do something
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
+
+  # def create
+  #   # do something
+  # end
 
   def set_user
 
     @user = User.find(params[:id])
   end
+  private
 
   def user_params
+
     # params.require(:user).permit(:name, :surname, :phone, :email, :balance, :added, :held)
   end
 end
