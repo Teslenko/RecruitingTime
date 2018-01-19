474c493b4f7c8b21730e68035bb21f9be00bd14f
root <root@DESKTOP-J7RCC6J.localdomain>
2018-01-19 13:35:36 +0200

    The SignUp a ready, but not hove account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+  # def after_sign_in_path_for(resource)
+  #   current_user
+  # end
+
   def after_sign_in_path_for(resource)
-    current_user_path
+    if current_user.has_attribute?(:admin)
+      dashboard_path
+    elsif current_user.has_attribute?(:users)
+      root_path
+    else
+      root_path
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
